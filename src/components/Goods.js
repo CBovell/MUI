11438afe6790274411996e466c5e131446650762
CBovell <venujaperera12@gmail.com>
2023-07-17 06:51:24 -0400

    form complete

--- src/components/Goods.js
+++ src/components/Goods.js
@@ -1,13 +1,13 @@
 import { useState } from "react"
-import { TextField } from "@mui/material"
+import { TextField, Slider, FormControl, InputLabel, Select, MenuItem, Button } from "@mui/material"
 import Footer from "./Footer"
 
 function Goods(){
 
     const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [tel, setTel] = useState('')
-    const [num, setNum] = useState('')
+    const [contact, setContact] = useState('')
+    const [type, setType] = useState('A')
+    const [num, setNum] = useState(12)
     const[imglinks, setImage] = useState('')
     const [description, setDescription] =useState('')
 
@@ -15,21 +15,66 @@ function Goods(){
         console.log(name)
     })
 
+    function valuetext(value) {
+        return {value};
+      }
+    
+    function handleSubmit(){
+        console.log('ch')
+    }
+
 
     return(
         <div className="goodswrapper">
             <div className="goods">
             <div className="inputForm">
             <div className="formtitles">
+            <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Type</InputLabel>
+            <Select
+            labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={type}
+                onChange={e =>{
+                    setType(e.target.value)
+                }}
+                >
+                <MenuItem value={'A'}>A</MenuItem>
+                <MenuItem value={'B'}>B</MenuItem>
+                <MenuItem value={'C'}>C</MenuItem>
+                <MenuItem value={'D'}>D</MenuItem>
+                </Select>
+        </FormControl>
+        <br/>
+        <br/>
+
+            <h3>Joe</h3>
+            </div>
+            <Slider
+            value={num}
+            name="number"
+            getAriaValueText={valuetext}
+            valueLabelDisplay="auto"
+            step={6}
+            marks
+            min={6}
+            max={100}
+            onChange={event => {
+                setNum(event.target.value);
+                console.log(num)
+                }}
+            />
+            <div className="formtitles">
             <h3>Joe</h3>
             </div>
             <TextField
             fullWidth
             id="outlined-controlled"
+            name="desc"
             value={name}
             onChange={event => {
             setName(event.target.value);
-            console.log(name)
+            console.log(description)
             }}
             />
             <br/>
@@ -40,13 +85,20 @@ function Goods(){
             <TextField
                 fullWidth
                 id="outlined-controlled"
-               
+                name="links"
                 value={name}
                 onChange={event => {
                 setName(event.target.value);
                 console.log(name)
                 }}
             />
+            <br/>
+            <br/>
+            <Button 
+            variant="text"
+            onClick={handleSubmit}
+            
+            >Submit</Button>
             </div>
         </div>
         <Footer/>
