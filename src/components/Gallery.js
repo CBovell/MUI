c0888880b2e4e0e34b6ec1a9543da186db637d62
CBovell <venujaperera12@gmail.com>
2023-07-21 02:55:02 -0400

    progress on form

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -56,6 +56,7 @@ function Gallery() {
   },[])
 
 
+
    if(loading){
     return (
     
--- src/components/Goods.js
+++ src/components/Goods.js
@@ -27,13 +27,15 @@ function Goods(){
 
     return(
         <div className="goodswrapper">
-            <div>
-                <form>
-                    <input type='number' name='number' min ='0' placeholder='Number of Servings' required />
+            <div className="goods">
+
+                <div className="inputForm">
+                <form className="form">
+                    <input type='number' name='number' min ='0' placeholder='Number of Servings' className="textbox" required />
                     <br/>
-                    <input type = 'text' name='contact' placeholder="Contact Information" required/>
+                    <input type = 'text' name='contact' placeholder="Contact Information" className="textbox" required/>
                     <br/>
-                    <select name='type' required>
+                    <select name='type' className="textbox" required>
                         <option>{process.env.REACT_APP_CAT1}</option>
                         <option>{process.env.REACT_APP_CAT2}</option>
                         <option>{process.env.REACT_APP_CAT3}</option>
@@ -47,6 +49,8 @@ function Goods(){
 
 
                 </form>
+
+                </div>
 
 
             </div>
